ddd7147f71cbcd850f8116cf52c40b7be50df387
Sven Soldin <soldinsven@gmail.com>
2021-02-04 13:20:54 +0100

    added styling to Profile page and ImageUpload component

--- src/components/image-upload/ImageUpload.component.tsx
+++ src/components/image-upload/ImageUpload.component.tsx
@@ -1,37 +1,62 @@
-import * as React from "react";
-import axios from "axios";
+import * as React from 'react';
+import axios from 'axios';
 
-import "./ImageUpload.styles.css";
+import './ImageUpload.styles.css';
 
 const ImageUpload = () => {
-	const [picture, setPicture] = React.useState<undefined | any>();
+  const [picture, setPicture] = React.useState<undefined | File>();
 
-	const onDrop = (e: React.ChangeEvent<any>) => {
-		setPicture(e.target.files[0]);
-	};
+  const onDrop = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    setPicture(e.target.files[0]);
+  };
 
-	const handleUpload = async () => {
-		const data = new FormData();
-		if (!picture) alert("Please select a picture");
-		data.append("attachment", picture);
-		try {
-			await axios.post(
-				`${process.env.REACT_APP_SERVER_URL}/users/profile/add`,
-				data,
-				{ withCredentials: true }
-			);
-			setPicture(undefined);
-		} catch (err) {
-			console.error(err);
-		}
-	};
+  const handleUpload = async (e: React.SyntheticEvent<any>) => {
+    e.preventDefault();
+    const data = new FormData();
+    if (!picture) return alert('Please select a picture');
+    data.append('attachment', picture);
+    try {
+      await axios.post(
+        `${process.env.REACT_APP_SERVER_URL}/users/profile/add`,
+        data,
+        { withCredentials: true }
+      );
+      setPicture(undefined);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-	return (
-		<div className="image-upload">
-			<input type="file" onChange={onDrop} />
-			<button onClick={handleUpload}>UPLOAD</button>
-		</div>
-	);
+  return (
+    <div className='image-upload'>
+      <p>Change profile picture</p>
+      <form className='picture-form' onSubmit={handleUpload}>
+        <label htmlFor='img' className='picture-input-label'>
+          {' '}
+          <img
+            src={
+              picture
+                ? URL.createObjectURL(picture)
+                : 'https://www.doctorlasercursos.com.br/uploads/avatars/2016/06/empty-avatar.jpg'
+            }
+            alt='uploaded'
+            height={80}
+            width={80}
+          />
+        </label>
+        <input
+          type='file'
+          onChange={onDrop}
+          id='img'
+          className='picture-input'
+        />
+        <button onClick={handleUpload} className='upload-button'>
+          Upload
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default ImageUpload;
--- src/pages/profile/Profile.page.tsx
+++ src/pages/profile/Profile.page.tsx
@@ -1,51 +1,65 @@
-import * as React from "react";
-import { useAuthState } from "../../context/index";
+import * as React from 'react';
+import { useAuthState } from '../../context/index';
 
-import ImageUpload from "../../components/image-upload/ImageUpload.component";
+import ImageUpload from '../../components/image-upload/ImageUpload.component';
+
+import './Profile.styles.css';
 
 const ProfilePage = () => {
-	const {
-		userDetails: { name, email },
-	} = useAuthState();
-	const [form, setForm] = React.useState({
-		name,
-		email,
-	});
+  const {
+    userDetails: { name, email },
+  } = useAuthState();
+  const [form, setForm] = React.useState({
+    name,
+    email,
+  });
 
-	const handleChange = (e: any) => {
-		e.preventDefault();
-		const { name, value } = e.target;
-		setForm({ ...form, [name]: value });
-	};
+  const handleChange = (e: any) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
 
-	const handleSubmit = async (e: any) => {
-		e.preventDefault();
-		// Make api call to update user info
-		try {
-		} catch (err) {}
-	};
-	return (
-		<div className="profile-page">
-			<form onSubmit={handleSubmit}>
-				<label htmlFor="name">Name:</label>
-				<input
-					type="text"
-					value={form.name}
-					name="name"
-					onChange={handleChange}
-				/>
-				<label htmlFor="email">Email:</label>
-				<input
-					type="text"
-					value={form.email}
-					name="email"
-					onChange={handleChange}
-				/>
-				<button type="submit">Save changes</button>
-			</form>
-			<ImageUpload />
-		</div>
-	);
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    // Make api call to update user info
+    try {
+    } catch (err) {}
+  };
+  return (
+    <div className='profile-page'>
+      <form onSubmit={handleSubmit} className='signin-form'>
+        <div className='form-field'>
+          <label htmlFor='name' className='label'>
+            Name:
+          </label>
+          <input
+            type='text'
+            value={form.name}
+            name='name'
+            onChange={handleChange}
+            className='input'
+          />
+        </div>
+        <div className='form-field'>
+          <label htmlFor='email' className='label'>
+            Email:
+          </label>
+          <input
+            type='text'
+            value={form.email}
+            name='email'
+            onChange={handleChange}
+            className='input'
+          />
+        </div>
+        <button type='submit' className='button'>
+          Save changes
+        </button>
+      </form>
+      <ImageUpload />
+    </div>
+  );
 };
 
 export default ProfilePage;
