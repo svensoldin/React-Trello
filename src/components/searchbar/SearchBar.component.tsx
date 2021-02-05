8c30a2b99dbbaeeb433aaf178c93cdd09cc5dcd9
Sven Soldin <soldinsven@gmail.com>
2021-02-05 16:20:38 +0100

    added responsive styles

--- src/components/searchbar/SearchBar.component.tsx
+++ src/components/searchbar/SearchBar.component.tsx
@@ -1,18 +1,18 @@
-import * as React from "react";
+import * as React from 'react';
 
-import SearchIcon from "@material-ui/icons/Search";
+import SearchIcon from '@material-ui/icons/Search';
 
-import "./Searchbar.styles.css";
+import './Searchbar.styles.css';
 
 const Searchbar = () => {
-	return (
-		<div className="searchbar">
-			<input type="text" placeholder="search..." className="search-input" />
-			<button className="search-btn">
-				<SearchIcon />
-			</button>
-		</div>
-	);
+  return (
+    <div className='searchbar'>
+      <input type='text' placeholder='search...' className='search-input' />
+      <button className='search-btn'>
+        <SearchIcon fontSize='small' />
+      </button>
+    </div>
+  );
 };
 
 export default Searchbar;
