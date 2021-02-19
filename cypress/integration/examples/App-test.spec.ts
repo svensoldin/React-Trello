e5c658c9b9890869c1b9fa7ef90fe5c374d273e0
Sven Soldin <soldinsven@gmail.com>
2021-02-19 18:06:57 +0100

    installed cypress, added two E2E tests on login flow

---
+++ cypress/integration/examples/App-test.spec.ts
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+it('authenticates the user with the right credentials', () => {
+  cy.visit('/');
+  cy.get('input[name=email]').type('john@gmail.com');
+  cy.contains('john@gmail.com');
+  cy.get('[type=password]').type('12345678{enter}');
+  cy.contains('My boards');
+});
+
+it('does not authenticate on wrong credentials and shows error message', () => {
+  cy.visit('/');
+  cy.get('[type=email]').type('jon@gmail.com');
+  cy.get('[type=password]').type('12345678{enter}');
+  cy.contains('Wrong credentials');
+});
